1389752aa297bef60cf07c029b37b86369c6bd97
AdarshQA <Adarshdkulkarni7570@gmail.com>
2026-06-26 08:31:22 +0530

    javascript learn

---
+++ samplewebautomaton/Javascriptlearning/variables/relationaloperators.js
@@ -0,0 +1,15 @@
+let x=25, y=50, z=25
+
+//relational operator
+console.log(y>x);
+console.log(y>=x)
+console.log(x<y)
+console.log(x<=y)
+console.log(x==z)
+console.log(x!=y)
+console.log(x===z)
+
+//logical operator
+console.log(y>x && y>z);
+console.log(!y>x && y>z);
+console.log(x>y||x>=z);
